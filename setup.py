63abacfca85302776537c457389c68dcf7cf0d56
LMMilliken <louis@djmilliken.co.uk>
2025-03-13 06:14:39 +0000

    update pyproject.toml

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
 
 setup(
-    packages=['sparse_probing_paper.' + pkg for pkg in find_packages(where="sparse_probing_paper")],
+    packages=find_packages(),
 )
